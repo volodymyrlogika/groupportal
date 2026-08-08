e0939a13ec0d8ea3e18dbefc86231638eb6a8a3d
Arsen5252 <sorochan.ars15@gmail.com>
2026-08-08 15:52:46 +0300

    create models

--- calendary/admin.py
+++ calendary/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_time', 'end_time', 'location')
+    list_filter = ('start_time', 'end_time', 'location')
+    search_fields = ('title', 'description')
