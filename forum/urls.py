a855210c85fce98719fc0b97f75ca47488269810
Roll0001 <arsenkov258@gmail.com>
2026-08-15 14:53:33 +0300

    Створив Модель

--- forum/urls.py
+++ forum/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Визнач тут свої URL-шляхи
+     path('', views.menu, name='menu'),
     
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def menu(request):
+    check_in = request.GET.get('check_in')
+    check_out = request.GET.get('check_out')
+
+    apartments = []
+    if check_in and check_out:
+        # Add real queryset logic here when an Apartment model is available.
+        apartments = []
+
+    return render(request, 'menu.html', {
+        'apartments': apartments,
+    })
