9d41990fd711b515d1eca4e953f17b80b4a7d779
Roll0001 <arsenkov258@gmail.com>
2026-08-22 16:09:44 +0300

    End

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
      path('', views.topic_list, name='topic_list'),
+     path('topic/new/', views.create_topic, name='create_topic'),
      path('topic_chat/<int:topic_id>/', views.topic_chat, name='topic_chat'),
+     path('post/<int:post_id>/like/', views.toggle_like, name='toggle_like'),
+     path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
+     path('post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
 ]
