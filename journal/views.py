586de75cfd0e0d1f1750f1e66c80400fb4e2786a
XxDimaAlfaXx <dimagarmatuk2010@gmail.com>
2026-08-22 15:09:30 +0300

    final

--- journal/views.py
+++ journal/views.py
@@ -4,6 +4,21 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Grade, Subject
 from .forms import GradeForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+
+class ModeratorMixin(LoginRequiredMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        if not request.user.groups.filter(name__in=['Модератори']).exists() and not request.user.is_superuser:
+            raise PermissionDenied("Ви не маєте доступу до цієї сторінки.")
+        return super().dispatch(request, *args, **kwargs)
+
+
+
+
+
 # Перегляд оцінок для учня
 class StudentJournalView(LoginRequiredMixin, ListView):
     model = Grade
@@ -14,7 +29,7 @@ class StudentJournalView(LoginRequiredMixin, ListView):
         return Grade.objects.filter(student=self.request.user)
 
 # Перегляд усіх оцінок для вчителя/адміна
-class AllGradesListView(LoginRequiredMixin, ListView):
+class AllGradesListView(ModeratorMixin, ListView):
     model = Grade
     template_name = 'journal/all_grades.html'
     context_object_name = 'grades'
@@ -43,14 +58,14 @@ class AllGradesListView(LoginRequiredMixin, ListView):
         return context
 
 # Створення оцінки (потрібен для urls.py)
-class GradeCreateView(LoginRequiredMixin, CreateView):
+class GradeCreateView(ModeratorMixin, CreateView):
     model = Grade
     form_class = GradeForm
     template_name = 'journal/grade_form.html'
     success_url = reverse_lazy('journal:all_grades')
 
 # Редагування оцінки
-class GradeUpdateView(LoginRequiredMixin, UpdateView):
+class GradeUpdateView(ModeratorMixin, UpdateView):
     model = Grade
     form_class = GradeForm
     template_name = 'journal/grade_form.html'
